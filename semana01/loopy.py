70bf07d619e919d8235bc4516d4bcbbf221bb8b0
João Nascimento <joantonio89@gmail.com>
2026-03-01 08:40:22 -0300

    Semana 01m- loops: tabuada interativa com lista histórico

--- semana01/loopy.py
+++ semana01/loopy.py
@@ -1,3 +1,4 @@
+'''
 # Exercício 1 - imprimir números de 1 a 10
 for i in range(1, 11):
     print(i)
@@ -17,3 +18,41 @@ for i in range(1, 101):
     soma += i
 
 print("Soma:", soma)
+
+
+# Exercício 4 - Tabuada 
+
+numero = int(input('Digite um número:'))
+for c in range(1,11):
+    resultado = c * numero
+    print(f'{numero} * {c} = {resultado}')
+
+# Exercício 4.1 - Tabuada com While
+
+numero = int(input('Digite um número (0 para sair): '))
+while numero != 0:
+    for c in range(1,11):
+        resultado = c * numero
+        print(f'{numero} * {c} = {resultado}')
+    numero = int(input('Digite um número (0 para sair): '))        
+
+'''
+
+# Exercício 4.2 - Tabuada 
+lista = []
+while True:
+    numero = int(input('Digite um número para mostrar a tabuada (0 para sair): '))
+    if numero == 0:
+        break
+    lista.append(numero)
+    for c in range(1,11):
+        resultado = numero * c
+        print(f'{numero} * {c} = {resultado}')
+    continua = (input('Deseja continuar? (s/n)')).lower()
+    if continua != 's':
+        break
+print(f'Você calculou as tabuadas dos números {lista}')    
+
+
+
+
