a6a94b32e0895f690810e2b23ee1ffef6cf0a4a6
João Nascimento <joantonio89@gmail.com>
2026-03-01 17:40:49 -0300

    Semana 01 - exercícios de loops concluídos (for, while e menu)

--- semana01/loopy.py
+++ semana01/loopy.py
@@ -1,58 +1,98 @@
 '''
+#===================================
 # Exercício 1 - imprimir números de 1 a 10
+#===================================
+
 for i in range(1, 11):
     print(i)
+print('Fim da contagem')
 
-print("-----")
-
+#===================================
 # Exercício 2 - imprimir apenas números pares
+#===================================
+
 for i in range(1, 21):
     if i % 2 == 0:
         print(i)
 
-print("-----")
+#===================================
+# Exercício 3 - Soma simples
+#===================================        
 
-# Exercício 3 - soma de 1 a 100
 soma = 0
-for i in range(1, 101):
-    soma += i
+for c in range (1, 11):
+    soma = soma + c
+    print(c)
+print(f'A soma total é {soma}')    
 
-print("Soma:", soma)
+#===================================
+# Exercício 4 - Loop com parada
+#===================================  
 
-
-# Exercício 4 - Tabuada 
-
-numero = int(input('Digite um número:'))
-for c in range(1,11):
-    resultado = c * numero
-    print(f'{numero} * {c} = {resultado}')
-
-# Exercício 4.1 - Tabuada com While
-
-numero = int(input('Digite um número (0 para sair): '))
-while numero != 0:
-    for c in range(1,11):
-        resultado = c * numero
-        print(f'{numero} * {c} = {resultado}')
-    numero = int(input('Digite um número (0 para sair): '))        
-
-'''
-
-# Exercício 4.2 - Tabuada 
-lista = []
 while True:
-    numero = int(input('Digite um número para mostrar a tabuada (0 para sair): '))
+    numero = int(input('Digite um número [0 para sair]: '))
     if numero == 0:
         break
-    lista.append(numero)
-    for c in range(1,11):
-        resultado = numero * c
-        print(f'{numero} * {c} = {resultado}')
-    continua = (input('Deseja continuar? (s/n)')).lower()
-    if continua != 's':
-        break
-print(f'Você calculou as tabuadas dos números {lista}')    
+print('Fim do programa...')  
 
+#===================================
+# Exercício 5 - Acumulador
+#===================================  
+
+total = 0
+while True:
+    numero = int(input('Digite um número para somar [0 para sair]: '))
+    if numero == 0:
+        break
+    total += numero
+print(f'A soma total dos número digitados é {total}')    
+
+#===================================
+# Exercício 6 - Contador
+#===================================  
+
+contador = 0
+while True:
+    numero = int(input('Digite um número [0 para sair]: '))
+    if numero == 0:
+        break
+    contador += 1
+print(f'A quantidade de números digitados foi de {contador}')    
+
+#===================================
+# Exercício 7 - Maior Número 
+#=================================== 
+
+maior = 0
+while True:
+    numero = int(input('Digite um número para validar o maior [0 para sair]: '))
+    if numero == 0:
+        break
+    if numero > maior:
+        maior = numero
+print(f'O maior número digitado foi {maior}')   
+    
+'''
+#===================================
+# Exercício 8 - Menu Simples
+#=================================== 
+
+while True:
+    print('1 - Tabuada')
+    print('2 - Sair')
+    opt = int(input('Digite uma opção: '))
+    if opt == 2:
+        break
+    elif opt == 1:
+        tabuada = int(input('Digite um número para apresentar a tabuada: '))
+        for c in range(1, 11):
+            resultado = tabuada * c
+            print(f'{tabuada} x {c} = {resultado}')
+
+    else:
+        print('Opção inválida, digite novamente')
+        
+            
 
 
 
